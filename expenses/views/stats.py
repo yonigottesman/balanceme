b0f89f4729f88c6afd11eef8fb9ff8535128fb24
Yonatan Gottesman <yonigo10@gmail.com>
2018-02-23 13:10:16 +0200

    fix manual stats input_source bug 3

--- expenses/views/stats.py
+++ expenses/views/stats.py
@@ -60,7 +60,7 @@ def stats_category(request, category):
     if end_date is not None:
         transactions = transactions.filter(date__lte=end_date)
         url_params = url_params + "&endDate=" + end_date.strftime('%Y-%m-%d')
-    if input_source is not None and input_source is not 'all':
+    if input_source is not None and input_source != 'all':
         url_params = url_params + "&source=" + input_source
         transactions = transactions.filter(source_id=int(input_source))
 
@@ -101,7 +101,7 @@ def category_piechart(request, start_date, end_date, search_text, input_source):
     if end_date is not None:
         transactions = transactions.filter(date__lte=end_date)
         url_params = url_params + "&endDate=" + end_date.strftime('%Y-%m-%d')
-    if input_source is not None and input_source is not 'all':
+    if input_source is not None and input_source != 'all':
         url_params = url_params + "&source=" + input_source
         transactions = transactions.filter(source_id=int(input_source))
 
